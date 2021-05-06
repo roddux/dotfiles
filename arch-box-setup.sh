fc41d834c27441ed57e3170912e6f2656798df40
Rory Mackie <roddux@gmail.com>
2021-05-06 17:21:37 +0100

    Updated auto-provision script

--- arch-box-setup.sh
+++ arch-box-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -Eeuo pipefail
 
-set -x
 # This script has 3 stages. The same script runs on the host, in the
 # pre-install environment within the VM and in the chroot-install environment
 # in the VM. The first argument to this script controls which stage executes.
@@ -16,265 +15,328 @@ NEW_CACHE="${NEW_CACHE:=yes}"
 # Set a new pacman mirrorlist (yes/no) [no]
 NEW_MIRRORLIST="${NEW_MIRRORLIST:=no}"
 
-VM="${VM=$(date +%d%b%y)}" # Default to ddMonthyy
+VM="${VM:=$(date +%d%b%y)}" # Default to ddMonthyy
 SCRIPT_NAME=$0
+ROOT_PASS="${ROOT_PASS:=root}" # default root pass
+USER_NAME="${USER_NAME:=user}" # default user name
+USER_PASS="${USER_PASS:=user}" # default user pass
 
 run_host() {
-# Config information
-cat <<E
-CACHE_FILEPATH: $CACHE_FILEPATH
-    CACHE_FILE: $CACHE_FILE
-     USE_CACHE: $USE_CACHE
-     NEW_CACHE: $NEW_CACHE
-NEW_MIRRORLIST: $NEW_MIRRORLIST
-   SCRIPT PATH: $0
-            VM: $VM
-E
+	# Config information
+	cat <<-E
+	CACHE_FILEPATH: $CACHE_FILEPATH
+	    CACHE_FILE: $CACHE_FILE
+	     USE_CACHE: $USE_CACHE
+	     NEW_CACHE: $NEW_CACHE
+	NEW_MIRRORLIST: $NEW_MIRRORLIST
+	   SCRIPT PATH: $0
+		    VM: $VM
+	E
 
-# Instruct user
-cat <<E
-#1: Boot your VM in UEFI mode and get into the Arch pre-install environment.
-#2: Set yourself a root password and make sure SSH is open to connections.
+	# Instruct user
+	cat <<-E
+	#1: Boot your VM in UEFI mode and get into the Arch pre-install environment.
+	#2: Set yourself a root password and make sure SSH is open to connections.
 
-Hit enter once ready. 
-E
+	Hit enter once ready. 
+	E
 
-read
+	read
 
-echo -n "Okay, now type your VM IP address: "
-read VMIP
+	echo -n "Okay, now type your VM IP address: "
+	read VMIP
 
-umask 077 # don't want anyone reading the keys
-TMPKEY=$(mktemp tmp-ssh-key-XXXXXX) # generate a name
-rm $TMPKEY # ssh-keygen will overwrite it anyway
+	umask 077 # don't want anyone reading the keys
+	TMPKEY=tmp-ssh-key-$RANDOM$RANDOM # generate a name
+	echo "Generating temporary SSH key"
+	ssh-keygen -N "" -t ecdsa -f $TMPKEY
 
-echo "Generating temporary SSH key"
-ssh-keygen -N "" -t ecdsa -f $TMPKEY
+	echo "Copying temporary SSH key to VM"
+	cat $TMPKEY.pub | ssh -o 'StrictHostKeyChecking accept-new' root@$VMIP -- "rm -rf /root/.ssh; mkdir /root/.ssh; cat >/root/.ssh/authorized_keys </dev/stdin"
 
-echo "Got ssh key: $TMPKEY"
-cat $TMPKEY
+	echo "Copying install script to VM"
+	scp -i $TMPKEY $SCRIPT_NAME root@$VMIP:/
 
-echo "Copying temporary SSH key to VM"
-cat $TMPKEY.pub | ssh -o 'StrictHostKeyChecking accept-new' root@$VMIP -- "rm -rf /root/.ssh; mkdir /root/.ssh; cat >/root/.ssh/authorized_keys </dev/stdin"
-
-echo "Copying install script to VM"
-scp -i $TMPKEY $SCRIPT_NAME root@$VMIP:/
-
-if [ "$USE_CACHE" == "yes" ]; then
-	echo "Checking for local package cache"
-	if [ -f "$CACHE_FILEPATH" ]; then
-		echo "Loading host package cache onto VM"
-		scp -i $TMPKEY $CACHE_FILEPATH root@$VMIP:/tmp/
-	else
-		echo "No local package cache available"
+	if [ "$USE_CACHE" == "yes" ]; then
+		echo "Checking for local package cache"
+		if [ -f "$CACHE_FILEPATH" ]; then
+			echo "Loading host package cache onto VM"
+			scp -i $TMPKEY $CACHE_FILEPATH root@$VMIP:/tmp/
+		else
+			echo "No local package cache available"
+		fi
 	fi
-fi
 
-echo "Running install script in VM"
-ssh -i $TMPKEY root@$VMIP -- NEW_MIRRORLIST=$NEW_MIRRORLIST USE_CACHE=$USE_CACHE CACHE_FILE=$CACHE_FILE NEW_CACHE=$NEW_CACHE /$SCRIPT_NAME preinstall
+	echo "Running install script in VM"
+	ssh -i $TMPKEY root@$VMIP -- NEW_MIRRORLIST=$NEW_MIRRORLIST USE_CACHE=$USE_CACHE CACHE_FILE=$CACHE_FILE NEW_CACHE=$NEW_CACHE USER_NAME=$USER_NAME USER_PASS=$USER_PASS ROOT_PASS=$ROOT_PASS /$SCRIPT_NAME preinstall
 
-if [ "$NEW_CACHE" == "yes" ]; then # download new package cache from vm
-	echo "Retrieving new package cache"
-	scp -i $TMPKEY root@$VMIP:/mnt/var/cache/pacman/$CACHE_FILE $CACHE_FILEPATH
-	echo "Rebooting VM" # if NEW_CACHE is no, VM reboots itself
-	ssh -i $TMPKEY root@$VMIP -- "reboot"
-fi
+	if [ "$NEW_CACHE" == "yes" ]; then # download new package cache from vm
+		echo "Retrieving new package cache"
+		scp -i $TMPKEY root@$VMIP:/mnt/var/cache/pacman/$CACHE_FILE $CACHE_FILEPATH
+		echo "Rebooting VM" # if NEW_CACHE is no, VM reboots itself
+		ssh -i $TMPKEY root@$VMIP -- "reboot"
+	fi
 
-echo "Removing temporary SSH keys from host"
-rm -f $TMPKEY $TMPKEY.pub
+	echo "Removing temporary SSH keys from host"
+	rm --force $TMPKEY $TMPKEY.pub
 }
 
 run_preinstall() {
-echo "=> Now in preinstall"
-echo "Setting up NTP"
-timedatectl set-ntp true
+	echo "=> Now in preinstall"
+	wall "archbox install script now running"
 
-echo "Unmounting disks (cleaning up, if re-running)"
-umount -R /mnt || true
+	echo "Setting up NTP"
+	timedatectl set-ntp true
 
-echo "Partitioning disk"
-echo -e ",100m\n;\n" | sfdisk -X gpt /dev/sda
+	echo "Unmounting disks (cleaning up, if re-running)"
+	umount --recursive /mnt || true
 
-echo "Waiting for kernel to recognise partition changes"
-while ! test -b /dev/sda1; do sleep 0.1; done
+	echo "Partitioning disk"
+	echo -e ",100m\n;\n" | sfdisk --label gpt /dev/sda
 
-echo "Formatting disk"
-mkfs.fat -F32 /dev/sda1 # /boot BIOS partition
-mkfs.xfs -f /dev/sda2   # /root XFS partition
+	echo "Waiting for kernel to recognise partition changes"
+	while ! test -b /dev/sda1; do sleep 0.1; done
 
-echo "Mounting partitions"
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+	echo "Formatting disk"
+	mkfs.fat -F32 /dev/sda1 # /boot EFI partition
+	mkfs.xfs -f /dev/sda2   # /root XFS partition
 
-if [ "$NEW_MIRRORLIST" == "yes" ]; then
-	echo "Updating mirror list"
-	reflector --latest 20 --sort rate --country "United Kingdom" --save /etc/pacman.d/mirrorlist
-else
-	echo "Setting mirror list"
-	cat <<E > /etc/pacman.d/mirrorlist
-Server = http://mirrors.manchester.m247.com/arch-linux/\$repo/os/\$arch
-Server = http://lon.mirror.rackspace.com/archlinux/\$repo/os/\$arch
-Server = https://lon.mirror.rackspace.com/archlinux/\$repo/os/\$arch
-Server = https://mirror.bytemark.co.uk/archlinux/\$repo/os/\$arch
-Server = http://mirror.bytemark.co.uk/archlinux/\$repo/os/\$arch
-Server = rsync://lon.mirror.rackspace.com/archlinux/\$repo/os/\$arch
-Server = rsync://mirror.bytemark.co.uk/archlinux/\$repo/os/\$arch
-Server = rsync://mirrors.manchester.m247.com/archlinux/\$repo/os/\$arch
-Server = http://www.mirrorservice.org/sites/ftp.archlinux.org/\$repo/os/\$arch
-Server = rsync://rsync.mirrorservice.org/ftp.archlinux.org/\$repo/os/\$arch
-Server = https://mirror.netweaver.uk/archlinux/\$repo/os/\$arch
-Server = https://www.mirrorservice.org/sites/ftp.archlinux.org/\$repo/os/\$arch
-Server = http://archlinux.mirrors.uk2.net/\$repo/os/\$arch
-Server = https://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch
-Server = rsync://mirrors.uk2.net/archlinux/\$repo/os/\$arch
-Server = http://mirror.netweaver.uk/archlinux/\$repo/os/\$arch
-Server = http://mirrors.ukfast.co.uk/sites/archlinux.org/\$repo/os/\$arch
-Server = rsync://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch
-Server = https://mirrors.ukfast.co.uk/sites/archlinux.org/\$repo/os/\$arch
-Server = http://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch
-E
-fi
+	echo "Chaing EFI partition type to make it bootable"
+	sfdisk --part-type /dev/sda 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 
-if [ "$USE_CACHE" == "yes" ]; then
-	if [ -f "/tmp/$CACHE_FILE" ]; then
-		echo "Found package cache"
-		mkdir -p /mnt/var/cache/pacman || true
-		mv /tmp/$CACHE_FILE /mnt/var/cache/pacman
-		pushd /mnt/var/cache/pacman
-		echo "Extracting package cache"
-		tar xf ./$CACHE_FILE
-		popd
+	echo "Setting arch-grub as first EFI-Boot option"
+	ARCH_EFI_NUM=$(efibootmgr | grep arch-grub | cut -d "*" -f1 | sed 's/Boot//')  # just in case it's not 0004
+	efibootmgr --active --bootnum $ARCH_EFI_NUM
+
+	echo "Mounting partitions"
+	mount /dev/sda2 /mnt
+	mkdir /mnt/boot
+	mount /dev/sda1 /mnt/boot
+
+	if [ "$NEW_MIRRORLIST" == "yes" ]; then
+		echo "Updating mirror list"
+		reflector --latest 10 --sort rate --country "United Kingdom" --save /etc/pacman.d/mirrorlist
 	else
-		echo "Told to use cache, but no cache file present!"
-		exit 1
+		echo "Setting mirror list" # 20210506
+		cat <<-"E" > /etc/pacman.d/mirrorlist
+			Server = https://archlinux.uk.mirror.allworldit.com/archlinux/$repo/os/$arch
+			Server = http://archlinux.uk.mirror.allworldit.com/archlinux/$repo/os/$arch
+			Server = http://mirrors.manchester.m247.com/arch-linux/$repo/os/$arch
+			Server = https://mirror.bytemark.co.uk/archlinux/$repo/os/$arch
+			Server = http://lon.mirror.rackspace.com/archlinux/$repo/os/$arch
+			Server = http://mirror.bytemark.co.uk/archlinux/$repo/os/$arch
+			Server = https://lon.mirror.rackspace.com/archlinux/$repo/os/$arch
+			Server = rsync://archlinux.uk.mirror.allworldit.com/archlinux/$repo/os/$arch
+			Server = rsync://mirror.bytemark.co.uk/archlinux/$repo/os/$arch
+			Server = rsync://mirrors.manchester.m247.com/archlinux/$repo/os/$arch
+		E
 	fi
-fi
 
-echo "Installing base packages"
-pacstrap /mnt \
-	base \
-	base-devel \
-	bind \
-	linux-lts \
-	linux-firmware \
-	neovim \
-	man-db \
-	man-pages \
-	grub \
-	efibootmgr \
-	xfsprogs \
-	dhcpcd \
-	mesa \
-	xorg-server \
-	xorg-server-common \
-	xf86-video-vmware \
-	xorg-xinit \
-	xorg-xrandr \
-	xorg-xrdb \
-	rxvt-unicode \
-	firefox \
-	chromium \
-	xterm \
-	i3-gaps \
-	i3status \
-	rofi \
-	python \
-	go \
-	git \
-	dmenu
+	if [ "$USE_CACHE" == "yes" ]; then
+		if [ -f "/tmp/$CACHE_FILE" ]; then
+			echo "Found package cache"
+			mkdir -p /mnt/var/cache/pacman || true
+			mv /tmp/$CACHE_FILE /mnt/var/cache/pacman
+			pushd /mnt/var/cache/pacman
+			echo "Extracting package cache"
+			tar xf ./$CACHE_FILE
+			popd
+		else
+			echo "Told to use cache, but no cache file present!"
+			exit 1
+		fi
+	fi
 
-echo "Generating fstab"
-genfstab -U /mnt > /mnt/etc/fstab
+	echo "Installing base packages"
+	pacstrap /mnt \
+		base \
+		base-devel \
+		bind \
+		linux-lts \
+		linux-firmware \
+		neovim \
+		man-db \
+		man-pages \
+		grub \
+		efibootmgr \
+		xfsprogs \
+		dhcpcd \
+		mesa \
+		xorg-server \
+		xorg-server-common \
+		xf86-video-vmware \
+		xorg-xinit \
+		xorg-xrandr \
+		xorg-xrdb \
+		rxvt-unicode \
+		firefox \
+		chromium \
+		xterm \
+		i3-gaps \
+		i3status \
+		rofi \
+		python \
+		go \
+		git \
+		open-vm-tools \
+		dmenu
 
-echo "Copying install script to chroot" 
-cp $SCRIPT_NAME /mnt/
+	echo "Generating fstab"
+	genfstab -U /mnt > /mnt/etc/fstab
 
-echo "Entering chroot"
-arch-chroot /mnt /$SCRIPT_NAME chroot
+	echo "Copying install script to chroot" 
+	cp $SCRIPT_NAME /mnt/
 
-echo "=> Back in preinstall" 
-echo "Synchronising filesystem changes" 
-sync
+	echo "Entering chroot"
+	cat <<-E >/mnt/config.ini
+	USER_NAME=$USER_NAME
+	USER_PASS=$USER_PASS
+	ROOT_PASS=$ROOT_PASS
+	E
+	arch-chroot /mnt /$SCRIPT_NAME chroot
 
-if [ "$NEW_CACHE" == "yes" ]; then
-	echo "Building new package cache"
-	pushd /mnt/var/cache/pacman/
-	tar cf $CACHE_FILE ./pkg/
-else
-	echo "Rebooting"
-	reboot
-fi
+	echo "=> Back in preinstall" 
+	echo "Synchronising filesystem changes" 
+	sync
+
+	if [ "$NEW_CACHE" == "yes" ]; then
+		echo "Building new package cache"
+		pushd /mnt/var/cache/pacman/
+		tar cf $CACHE_FILE ./pkg/
+	else
+		echo "Rebooting"
+		reboot
+	fi
 }
 
 run_chroot() {
-echo "=> Now in chroot"
-echo "Setting timezone"
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock --systohc
-timedatectl set-ntp true
+	echo "=> Now in chroot"
+	echo "Sourcing configuration"
+	source /config.ini
 
-echo "Setting locale"
-cat <<E > /etc/locale.gen
-en_GB.UTF-8 UTF-8
-E
+	echo "Setting timezone"
+	ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+	hwclock --systohc
+	timedatectl set-ntp true
 
-locale-gen
+	echo "Setting locale"
+	cat <<-E > /etc/locale.gen
+	en_GB.UTF-8 UTF-8
+	E
 
-cat <<E > /etc/locale.conf
-LANG=en_GB.UTF-8
-E
+	locale-gen
 
-echo "Setting hostname"
-echo $VM > /etc/hostname
-hostnamectl set-hostname $VM
+	cat <<-E > /etc/locale.conf
+	LANG=en_GB.UTF-8
+	E
 
-echo "Setting up hosts file"
-cat <<E > /etc/hosts
-127.0.0.1	localhost
-::1		localhost
-127.0.0.1	$VM.localdomain $VM
-E
+	echo "Setting hostname"
+	echo $VM > /etc/hostname
+	hostnamectl set-hostname $VM
 
-echo "Installing bootloader"
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch-grub
-grub-mkconfig -o /boot/grub/grub.cfg
+	echo "Setting up hosts file"
+	cat <<-E > /etc/hosts
+	127.0.0.1	localhost
+	::1		localhost
+	127.0.0.1	$VM.localdomain $VM
+	E
 
-echo "Adding user 'user'"
-useradd -m user
+	echo "Installing bootloader"
+	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch-grub
+	grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Setting passwords"
-echo 'user:user' | chpasswd
-echo 'root:root' | chpasswd
+	echo "Adding user '$USER_NAME'"
+	useradd --create-home $USER_NAME
 
-# TODO: open-vm-tools service, firefox/chrome ublock addin
+	echo "Setting passwords"
+	echo "$USER_NAME:$USER_PASS" | chpasswd
+	echo "root:$ROOT_PASS" | chpasswd
 
-# Setup dotfiles
-echo "Downloading dotfiles"
-pushd /tmp
-git clone https://github.com/roddux/dotfiles.git
-pushd dotfiles/vmware
-echo "Copying vmware dotfiles"
-cp -r ./.* ./* /home/user/
-popd; pushd dotfiles/common
-echo "Copying common dotfiles"
-cp -r ./.* ./* /home/user/
-popd
+	echo "Giving $USER_NAME password-less sudo"
+	cat <<-E >/etc/sudoers.d/$USER_NAME.sudo
+	$USER_NAME ALL=(ALL) NOPASSWD: ALL
+	E
+
+	echo "Downloading dotfiles"
+	pushd /tmp
+	git clone https://github.com/roddux/dotfiles.git
+	echo "Copying dotfiles"
+	cp --verbose --recursive --no-target-directory dotfiles/common /home/$USER_NAME/
+	cp --verbose --recursive --no-target-directory dotfiles/vmware-vm /home/$USER_NAME/
+	mkdir -p /home/$USER_NAME/.config/i3/
+	mv /home/$USER_NAME/i3config /home/$USER_NAME/.config/i3/config
+	chown -R $USER_NAME.$USER_NAME /home/$USER_NAME/
+	popd
+
+	echo "Setting up getty override to enable autologin"
+	mkdir -p /etc/systemd/system/getty@.service.d/
+	cat <<-"E" >/etc/systemd/system/getty@.service.d/override.conf
+	[Service]
+	ExecStart=
+	ExecStart=-/sbin/agetty -a $USER_NAME --noclear %I $TERM
+	E
+
+	echo "Setting up dhcpcd"
+	mkdir -p /etc/systemd/system/dhcpcd.service.d/ 
+	cat <<-E >/etc/systemd/system/dhcpcd.service.d/override.conf 
+	[Service]
+	ExecStart=
+	ExecStart=/usr/bin/dhcpcd --background --ipv4only --noarp --quiet
+	E
+	systemctl enable dhcpcd
+
+	echo "Setting up vmware-specific services"
+	cat <<-E >/etc/systemd/system/vmware-vmblock-fuse.service
+	[Unit]
+	Description=Open Virtual Machine Tools (vmware-vmblock-fuse)
+	ConditionVirtualization=vmware
+	After=vmtoolsd
+
+	[Service]
+	Type=simple
+	RuntimeDirectory=vmblock-fuse
+	RuntimeDirectoryMode=755
+	ExecStart=/usr/bin/vmware-vmblock-fuse -d -f -o subtype=vmware-vmblock,default_permissions,allow_other /run/vmblock-fuse
+
+	[Install]
+	WantedBy=multi-user.target
+	E
+
+	cat <<-E >/etc/systemd/system/vmtoolsd.service
+	[Unit]
+	Description=Open Virtual Machine Tools (VMware Tools)
+	ConditionVirtualization=vmware
+	After=display-manager.service
+
+	[Service]
+	ExecStart=/usr/bin/vmtoolsd
+
+	[Install]
+	WantedBy=multi-user.target
+	E
+	systemctl enable vmtoolsd
+	systemctl enable vmware-vmblock-fuse
+	chmod 440 /etc/systemd/system/vmtoolsd.service
+	chmod 440 /etc/systemd/system/vmware-vmblock-fuse.service
 }
 
-if [ $# -eq 0 ]; then
-	MODE="host"
-elif [ $# -eq 1 ]; then
-	MODE="$1"
-fi
+main() {
+	if [ $# -eq 0 ]; then
+		MODE="host"
+	elif [ $# -eq 1 ]; then
+		MODE="$1"
+	fi
 
-if [ "$MODE" == "host" ]; then
-	run_host
-elif [ "$MODE" == "preinstall" ]; then
-	run_preinstall
-elif [ "$MODE" == "chroot" ]; then
-	run_chroot
-else
-	echo "Invalid argument"
-	exit 1
-fi
+	if [ "$MODE" == "host" ]; then
+		run_host
+	elif [ "$MODE" == "preinstall" ]; then
+		run_preinstall
+	elif [ "$MODE" == "chroot" ]; then
+		run_chroot
+	else
+		echo "Invalid argument"
+		exit 1
+	fi
+}
+
+main $@
