0e7cfb3b7d0adcd0aedbe94fe70482ed5e271ad3
roddux <roddux@gmail.com>
2021-07-06 10:11:43 +0100

    Script updates, fixes

--- arch-box-setup.sh
+++ arch-box-setup.sh
@@ -1,44 +1,56 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -Eeuo pipefail
-
 # This script has 3 stages. The same script runs on the host, in the
 # pre-install environment within the VM and in the chroot-install environment
 # in the VM. The first argument to this script controls which stage executes.
 
 # These can be overridden by setting an environment variable of the same name
-CACHE_FILEPATH="${CACHE_FILEPATH:=~/.package_cache.tar}"
+if [ -z "${CACHE_FILEPATH:-}" ]; then CACHE_FILEPATH=~/.package_cache.tar; fi
 CACHE_FILE=$(basename $CACHE_FILEPATH)
-# Cache downloaded packages in the guest onto the host (yes/no) [yes]
-USE_CACHE="${USE_CACHE:=yes}"
-# Collect package cache from VM and update host package cache (yes/no) [yes]
-NEW_CACHE="${NEW_CACHE:=yes}"
-# Set a new pacman mirrorlist (yes/no) [no]
-NEW_MIRRORLIST="${NEW_MIRRORLIST:=no}"
 
-VM="${VM:=$(date +%d%b%y)}" # Default to ddMonthyy
+# Use a pre-generated package cache to save download/install time (yes/no) [yes]
+if [ -z "${USE_CACHE:-}" ]; then USE_CACHE=yes; fi
+
+# Generate a new package cache (yes/no) [yes]
+if [ -z "${NEW_CACHE:-}" ]; then NEW_CACHE=yes; fi
+
+# Generate a new pacman mirrorlist (yes/no) [no]
+if [ -z "${NEW_MIRRORLIST:-}" ]; then NEW_MIRRORLIST=no; fi
+
 SCRIPT_NAME=$0
-ROOT_PASS="${ROOT_PASS:=root}" # default root pass
-USER_NAME="${USER_NAME:=user}" # default user name
-USER_PASS="${USER_PASS:=user}" # default user pass
+if [ -z "${VM:-}" ]; then VM=$(date +%d%b%y); fi # Default to ddMonthyy
+if [ -z "${ROOT_PASS:-}" ]; then ROOT_PASS=root; fi
+if [ -z "${USER_NAME:-}" ]; then USER_NAME=user; fi
+if [ -z "${USER_PASS:-}" ]; then USER_PASS=user; fi
 
 run_host() {
 	# Config information
-	cat <<-E
-	CACHE_FILEPATH: $CACHE_FILEPATH
-	    CACHE_FILE: $CACHE_FILE
-	     USE_CACHE: $USE_CACHE
-	     NEW_CACHE: $NEW_CACHE
-	NEW_MIRRORLIST: $NEW_MIRRORLIST
-	   SCRIPT PATH: $0
-		    VM: $VM
-	E
+	if [ $USE_CACHE == "yes" ]; then
+		cat <<-E
+			CACHE_FILEPATH: $CACHE_FILEPATH
+			    CACHE_FILE: $CACHE_FILE
+			     USE_CACHE: $USE_CACHE
+			     NEW_CACHE: $NEW_CACHE
+			NEW_MIRRORLIST: $NEW_MIRRORLIST
+			   SCRIPT PATH: $0
+			            VM: $VM
+		E
+	else
+		cat <<-E
+			     USE_CACHE: $USE_CACHE
+			     NEW_CACHE: $NEW_CACHE
+			NEW_MIRRORLIST: $NEW_MIRRORLIST
+			   SCRIPT PATH: $0
+			            VM: $VM
+		E
+	fi
 
 	# Instruct user
 	cat <<-E
-	#1: Boot your VM in UEFI mode and get into the Arch pre-install environment.
-	#2: Set yourself a root password and make sure SSH is open to connections.
+		#1: Boot your VM in UEFI mode and get into the Arch pre-install environment.
+		#2: Set yourself a root password and make sure SSH is open to connections.
 
-	Hit enter once ready. 
+		Hit enter once ready. 
 	E
 
 	read
@@ -49,36 +61,42 @@ run_host() {
 	umask 077 # don't want anyone reading the keys
 	TMPKEY=tmp-ssh-key-$RANDOM$RANDOM # generate a name
 	echo "Generating temporary SSH key"
-	ssh-keygen -N "" -t ecdsa -f $TMPKEY
+	ssh-keygen -N "" -t ecdsa -f $TMPKEY &>/dev/null
 
 	echo "Copying temporary SSH key to VM"
-	cat $TMPKEY.pub | ssh -o 'StrictHostKeyChecking accept-new' root@$VMIP -- "rm -rf /root/.ssh; mkdir /root/.ssh; cat >/root/.ssh/authorized_keys </dev/stdin"
+	cat $TMPKEY.pub | \
+		ssh -o 'StrictHostKeyChecking accept-new' root@$VMIP -- \
+			"rm -rf /root/.ssh; mkdir /root/.ssh; cat >/root/.ssh/authorized_keys </dev/stdin"
 
 	echo "Copying install script to VM"
 	scp -i $TMPKEY $SCRIPT_NAME root@$VMIP:/
 
 	if [ "$USE_CACHE" == "yes" ]; then
 		echo "Checking for local package cache"
-		if [ -f "$CACHE_FILEPATH" ]; then
+		echo $CACHE_FILEPATH
+		if [ -f $CACHE_FILEPATH ]; then
 			echo "Loading host package cache onto VM"
 			scp -i $TMPKEY $CACHE_FILEPATH root@$VMIP:/tmp/
 		else
 			echo "No local package cache available"
+			exit 1
 		fi
 	fi
 
 	echo "Running install script in VM"
-	ssh -i $TMPKEY root@$VMIP -- NEW_MIRRORLIST=$NEW_MIRRORLIST USE_CACHE=$USE_CACHE CACHE_FILE=$CACHE_FILE NEW_CACHE=$NEW_CACHE USER_NAME=$USER_NAME USER_PASS=$USER_PASS ROOT_PASS=$ROOT_PASS /$SCRIPT_NAME preinstall
+	ssh -i $TMPKEY root@$VMIP -- \
+		NEW_MIRRORLIST=$NEW_MIRRORLIST USE_CACHE=$USE_CACHE CACHE_FILE=$CACHE_FILE NEW_CACHE=$NEW_CACHE USER_NAME=$USER_NAME USER_PASS=$USER_PASS ROOT_PASS=$ROOT_PASS /$SCRIPT_NAME preinstall
 
 	if [ "$NEW_CACHE" == "yes" ]; then # download new package cache from vm
 		echo "Retrieving new package cache"
 		scp -i $TMPKEY root@$VMIP:/mnt/var/cache/pacman/$CACHE_FILE $CACHE_FILEPATH
-		echo "Rebooting VM" # if NEW_CACHE is no, VM reboots itself
-		ssh -i $TMPKEY root@$VMIP -- "reboot"
 	fi
 
+	echo "Rebooting VM"
+	ssh -i $TMPKEY root@$VMIP -- "reboot"
+
 	echo "Removing temporary SSH keys from host"
-	rm --force $TMPKEY $TMPKEY.pub
+	rm -f $TMPKEY $TMPKEY.pub
 }
 
 run_preinstall() {
@@ -91,7 +109,7 @@ run_preinstall() {
 	echo "Unmounting disks (cleaning up, if re-running)"
 	umount --recursive /mnt || true
 
-	echo "Partitioning disk"
+	echo "Partitioning disk" # 100mb efi partition, then the rest as main partition
 	echo -e ",100m\n;\n" | sfdisk --label gpt /dev/sda
 
 	echo "Waiting for kernel to recognise partition changes"
@@ -103,10 +121,6 @@ run_preinstall() {
 
 	echo "Chaing EFI partition type to make it bootable"
 	sfdisk --part-type /dev/sda 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-
-	echo "Setting arch-grub as first EFI-Boot option"
-	ARCH_EFI_NUM=$(efibootmgr | grep arch-grub | cut -d "*" -f1 | sed 's/Boot//')  # just in case it's not 0004
-	efibootmgr --active --bootnum $ARCH_EFI_NUM
 
 	echo "Mounting partitions"
 	mount /dev/sda2 /mnt
@@ -140,6 +154,7 @@ run_preinstall() {
 			pushd /mnt/var/cache/pacman
 			echo "Extracting package cache"
 			tar xf ./$CACHE_FILE
+			rm ./$CACHE_FILE
 			popd
 		else
 			echo "Told to use cache, but no cache file present!"
@@ -175,23 +190,27 @@ run_preinstall() {
 		i3-gaps \
 		i3status \
 		rofi \
+		ripgrep \
+		fd \
 		python \
 		go \
+		openssh \
 		git \
 		open-vm-tools \
+		gtkmm3 \
 		dmenu
 
-	echo "Generating fstab"
-	genfstab -U /mnt > /mnt/etc/fstab
+	echo "Generating fstab" # no fsck, no atime
+	genfstab -U /mnt | sed 's/0 [0-9]$/0 0/g;s/relatime/noatime/g' > /mnt/etc/fstab
 
 	echo "Copying install script to chroot" 
 	cp $SCRIPT_NAME /mnt/
 
 	echo "Entering chroot"
 	cat <<-E >/mnt/config.ini
-	USER_NAME=$USER_NAME
-	USER_PASS=$USER_PASS
-	ROOT_PASS=$ROOT_PASS
+		USER_NAME=$USER_NAME
+		USER_PASS=$USER_PASS
+		ROOT_PASS=$ROOT_PASS
 	E
 	arch-chroot /mnt /$SCRIPT_NAME chroot
 
@@ -199,20 +218,21 @@ run_preinstall() {
 	echo "Synchronising filesystem changes" 
 	sync
 
+	echo "Setting arch-grub as first EFI-Boot option"
+	ARCH_EFI_NUM=$(efibootmgr|grep arch-grub|sed 's/Boot//;s/\*.*//') 
+	efibootmgr --active --bootnum $ARCH_EFI_NUM
+
 	if [ "$NEW_CACHE" == "yes" ]; then
 		echo "Building new package cache"
 		pushd /mnt/var/cache/pacman/
 		tar cf $CACHE_FILE ./pkg/
-	else
-		echo "Rebooting"
-		reboot
 	fi
 }
 
 run_chroot() {
 	echo "=> Now in chroot"
 	echo "Sourcing configuration"
-	source /config.ini
+	source /config.ini && rm /config.ini
 
 	echo "Setting timezone"
 	ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
@@ -220,15 +240,10 @@ run_chroot() {
 	timedatectl set-ntp true
 
 	echo "Setting locale"
-	cat <<-E > /etc/locale.gen
-	en_GB.UTF-8 UTF-8
-	E
-
+	echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
 	locale-gen
 
-	cat <<-E > /etc/locale.conf
-	LANG=en_GB.UTF-8
-	E
+	echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 
 	echo "Setting hostname"
 	echo $VM > /etc/hostname
@@ -236,14 +251,16 @@ run_chroot() {
 
 	echo "Setting up hosts file"
 	cat <<-E > /etc/hosts
-	127.0.0.1	localhost
-	::1		localhost
-	127.0.0.1	$VM.localdomain $VM
+		127.0.0.1	localhost
+		::1		localhost
+		127.0.0.1	$VM.localdomain $VM
 	E
 
 	echo "Installing bootloader"
+	sed 's/^GRUB_TIMEOUT=./GRUB_TIMEOUT=0/g;s/^GRUB_CMDLINE_LINUX_DEF.*/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet mitigations=off"/g' -i /etc/default/grub
 	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch-grub
 	grub-mkconfig -o /boot/grub/grub.cfg
+	sed -i 's/  set timeout=5/  set timeout=1/g' /boot/grub/grub.cfg
 
 	echo "Adding user '$USER_NAME'"
 	useradd --create-home $USER_NAME
@@ -253,9 +270,7 @@ run_chroot() {
 	echo "root:$ROOT_PASS" | chpasswd
 
 	echo "Giving $USER_NAME password-less sudo"
-	cat <<-E >/etc/sudoers.d/$USER_NAME.sudo
-	$USER_NAME ALL=(ALL) NOPASSWD: ALL
-	E
+	echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/$USER_NAME
 
 	echo "Downloading dotfiles"
 	pushd /tmp
@@ -270,54 +285,60 @@ run_chroot() {
 
 	echo "Setting up getty override to enable autologin"
 	mkdir -p /etc/systemd/system/getty@.service.d/
-	cat <<-"E" >/etc/systemd/system/getty@.service.d/override.conf
-	[Service]
-	ExecStart=
-	ExecStart=-/sbin/agetty -a $USER_NAME --noclear %I $TERM
+	cat <<-E >/etc/systemd/system/getty@.service.d/override.conf
+		[Service]
+		ExecStart=
+		ExecStart=-/sbin/agetty -a $USER_NAME --noclear %I \$TERM
 	E
 
 	echo "Setting up dhcpcd"
 	mkdir -p /etc/systemd/system/dhcpcd.service.d/ 
 	cat <<-E >/etc/systemd/system/dhcpcd.service.d/override.conf 
-	[Service]
-	ExecStart=
-	ExecStart=/usr/bin/dhcpcd --background --ipv4only --noarp --quiet
+		[Service]
+		ExecStart=
+		ExecStart=/usr/bin/dhcpcd --background --ipv4only --noarp --quiet
 	E
 	systemctl enable dhcpcd
 
 	echo "Setting up vmware-specific services"
 	cat <<-E >/etc/systemd/system/vmware-vmblock-fuse.service
-	[Unit]
-	Description=Open Virtual Machine Tools (vmware-vmblock-fuse)
-	ConditionVirtualization=vmware
-	After=vmtoolsd
+		[Unit]
+		Description=Open Virtual Machine Tools (vmware-vmblock-fuse)
+		ConditionVirtualization=vmware
+		After=vmtoolsd
 
-	[Service]
-	Type=simple
-	RuntimeDirectory=vmblock-fuse
-	RuntimeDirectoryMode=755
-	ExecStart=/usr/bin/vmware-vmblock-fuse -d -f -o subtype=vmware-vmblock,default_permissions,allow_other /run/vmblock-fuse
+		[Service]
+		Type=simple
+		RuntimeDirectory=vmblock-fuse
+		RuntimeDirectoryMode=755
+		ExecStart=/usr/bin/vmware-vmblock-fuse -d -f -o subtype=vmware-vmblock,default_permissions,allow_other /run/vmblock-fuse
 
-	[Install]
-	WantedBy=multi-user.target
+		[Install]
+		WantedBy=multi-user.target
 	E
 
 	cat <<-E >/etc/systemd/system/vmtoolsd.service
-	[Unit]
-	Description=Open Virtual Machine Tools (VMware Tools)
-	ConditionVirtualization=vmware
-	After=display-manager.service
+		[Unit]
+		Description=Open Virtual Machine Tools (VMware Tools)
+		ConditionVirtualization=vmware
+		After=display-manager.service
 
-	[Service]
-	ExecStart=/usr/bin/vmtoolsd
+		[Service]
+		ExecStart=/usr/bin/vmtoolsd
 
-	[Install]
-	WantedBy=multi-user.target
+		[Install]
+		WantedBy=multi-user.target
 	E
 	systemctl enable vmtoolsd
 	systemctl enable vmware-vmblock-fuse
 	chmod 440 /etc/systemd/system/vmtoolsd.service
 	chmod 440 /etc/systemd/system/vmware-vmblock-fuse.service
+
+	echo "Trimming unused packages"
+	echo -e "Y\nY\n" | pacman -Sc
+
+	echo "Disabling pcspkr"
+	echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
 }
 
 main() {
